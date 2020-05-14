c5a4143da1e4d8160e6ca9dbed73794477a2de5b
Filippo Finke <filippo.finke@samtrevano.ch>
2020-05-14 16:59:57 +0200

    Aggiunti Middlewares di accesso

---
+++ 5_Sito o applicativo/src/Middlewares/AdminRequired.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace FilippoFinke\Middlewares;
+
+use FilippoFinke\Libs\Permission;
+
+/**
+ * AdminRequired.php
+ * Classe utilizzata per controllare se un utente
+ * ha l'accesso a determinati percorsi come utente amministratore.
+ *
+ * @author Filippo Finke
+ */
+class AdminRequired
+{
+    /**
+     * Controlla se l'utente è un admin.
+     *
+     * @param $request La richiesta.
+     * @param $response La risposta.
+     */
+    public function __invoke($request, $response)
+    {
+        if (!Permission::isAdmin()) {
+            $response->redirect("/login");
+            exit;
+        }
+    }
+}
