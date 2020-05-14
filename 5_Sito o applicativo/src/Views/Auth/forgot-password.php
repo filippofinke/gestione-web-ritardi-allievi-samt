e4c780b53e7e73c7ff77d5bc7f39bf09e68b6ceb
Filippo Finke <filippo.finke@samtrevano.ch>
2020-05-14 16:59:41 +0200

    Aggiunte pagine di basilari

---
+++ 5_Sito o applicativo/src/Views/Auth/forgot-password.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forgot Password</title>
+</head>
+<body>
+    <h1>Forgot Password</h1>
+    <a href="/login">Accedi</a>
+</body>
+</html>
---
+++ 5_Sito o applicativo/src/Views/Auth/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <a href="/forgot-password">Password dimenticata</a>
+
+    <form action="/login" method="post">
+        <input type="email" name="email" placeholder="Email">
+        <input type="password" name="password" placeholder="Password">
+        <button>Accedi</button>
+    </form>
+     
+</body>
+</html>
