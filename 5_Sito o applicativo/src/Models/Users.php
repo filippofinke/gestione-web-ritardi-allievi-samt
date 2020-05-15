346a0f4823fed1fa5a4476505f622556a30626e1
Filippo Finke <filippo.finke@samtrevano.ch>
2020-05-15 16:21:28 +0200

    Aggiunto model users

--- 5_Sito o applicativo/src/Models/Users.php
+++ 5_Sito o applicativo/src/Models/Users.php
@@ -52,4 +52,95 @@ class Users
         }
     }
 
+    /**
+     * Metodo utilizzato per inserire un utente.
+     *
+     * @param $name Il nome.
+     * @param $lastName Il cognome.
+     * @param $email L'indirizzo email.
+     * @return bool True oppure false.
+     */
+    public static function insert($email, $name, $lastname)
+    {
+        $pdo = Database::getConnection();
+        $query = "INSERT INTO user VALUES(:email, :name, :lastname, '', 0)";
+        $stm = $pdo->prepare($query);
+        $stm->bindParam(':email', strtolower($email));
+        $stm->bindParam(':name', ucfirst($name));
+        $stm->bindParam(':lastname', ucfirst($lastname));
+        try {
+            return $stm->execute() && Tokens::sendActivationToken($email);
+        } catch (\PDOException $e) {
+            throw new Exception("Un utente con questa email esiste già!");
+        }
+    }
+
+    /**
+     * Metodo utilizzato per aggiornare il permesso di un utente usando la sua
+     * email.
+     *
+     * @param $email L'indirizzo email dell'utente.
+     * @param $permission Il nuovo permesso.
+     * @return bool True oppure false.
+     */
+    public static function update($email, $permission)
+    {
+        $pdo = Database::getConnection();
+        $query = "UPDATE user SET permission = :permission WHERE email = :email";
+        $stm = $pdo->prepare($query);
+        $stm->bindParam(':email', $email);
+        $stm->bindParam(':permission', $permission);
+        try {
+            return $stm->execute();
+        } catch (\PDOException $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Metodo utilizzato per eliminare un utente usando la sua
+     * email.
+     *
+     * @param $email L'indirizzo email da cercare.
+     * @return bool True oppure false.
+     */
+    public static function delete($email)
+    {
+        $pdo = Database::getConnection();
+        $query = "DELETE FROM user WHERE email = :email";
+        $stm = $pdo->prepare($query);
+        $stm->bindParam(':email', $email);
+        try {
+            return $stm->execute();
+        } catch (\PDOException $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Metodo utilizzato per cambiare la password di un utente con un token.
+     *
+     * @param $token Il token di recupero password.
+     * @param $password La nuova password.
+     * @return bool True oppure false.
+     */
+    public static function changePassword($token, $password)
+    {
+        if (Validator::isValidPassword($password)) {
+            $email = Tokens::useToken($token);
+            if ($email) {
+                $pdo = Database::getConnection();
+                $query = "UPDATE user SET password = :password WHERE email = :email";
+                $stm = $pdo->prepare($query);
+                $stm->bindParam(':email', $email);
+                $stm->bindParam(':password', password_hash($password, PASSWORD_DEFAULT));
+                try {
+                    $_SESSION["login_email"] = $email;
+                    return $stm->execute();
+                } catch (\PDOException $e) {
+                }
+            }
+        }
+        return false;
+    }
 }
