759137db2fb2940eddb6574d89bfb7e89c38c908
Filippo Finke <filippo.finke@samtrevano.ch>
2020-05-18 09:30:04 +0200

    Aggiunto validators isValidSemester e isValidSection

--- 5_Sito o applicativo/src/Libs/Validator.php
+++ 5_Sito o applicativo/src/Libs/Validator.php
@@ -35,6 +35,37 @@ class Validator
     }
 
     /**
+     * Metodo che controlla se il nome di una sezione è valido.
+     *
+     * @param $section Il testo da controllare.
+     * @return bool True se il testo è valido altrimento false.
+     */
+    public static function isValidSection($section)
+    {
+        return preg_match('/^[A-Za-z0-9 ]{1,10}$/', $section);
+    }
+
+    /**
+     * Metodo che controlla se un anno scolastico è valido.
+     *
+     * @param $start_first_date Data di inizio primo semestre.
+     * @param $end_first_date Data di fine primo semestre.
+     * @param $start_second_date Data di inizio secondo semestre.
+     * @param $end_second_date  Data di fine secondo semestre.
+     * @return bool True se l'anno è valido altrimento false.
+     */
+    public static function isValidSemester($start_first_date, $end_first_date, $start_second_date, $end_second_date)
+    {
+
+        $start_first_date = strtotime($start_first_date);
+        $end_first_date = strtotime($end_first_date);
+        $start_second_date = strtotime($start_second_date);
+        $end_second_date = strtotime($end_second_date);
+
+        return $start_first_date < $end_first_date && $start_second_date > $end_first_date && $start_second_date < $end_second_date;
+    }
+
+    /**
      * Metodo che controlla se del testo è una password valida.
      *
      * @param $password Il testo da controllare.
@@ -51,9 +82,15 @@ class Validator
      * @param $email Il testo da controllare.
      * @return bool True se il testo è valido altrimento false.
      */
-    public static function isValidEmail($email)
+    public static function isValidEmail($email, $valid_domain = null)
     {
-        return filter_var($email, FILTER_VALIDATE_EMAIL);
+        $valid = filter_var($email, FILTER_VALIDATE_EMAIL);
+        if ($valid && $valid_domain) {
+            $parts = explode('@', $email);
+            $domain = array_pop($parts);
+            return $domain == $valid_domain;
+        }
+        return $valid;
     }
 
     /**
