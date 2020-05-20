3bab9c29cfdccd0cc7a3b98bfb160107421e5d00
Filippo Finke <filippo.finke@samtrevano.ch>
2020-05-20 08:31:19 +0200

    Aggiornato commento

--- 5_Sito o applicativo/config.sample.php
+++ 5_Sito o applicativo/config.sample.php
@@ -19,5 +19,5 @@ define("DB_HOST", "127.0.0.1");
 define("DB_NAME", "ritardi");
 // L'username dell'utente da utilizzare per collegarsi al database.
 define("DB_USERNAME", "USERNAME");
-// La password da utilizzarre per il collegamento al database.
+// La password da utilizzare per il collegamento al database.
 define("DB_PASSWORD", "PASSWORD");
