fad62e3686e6a3f15e2b711c12a0c0868152ecf9
Filippo Finke <filippo.finke@samtrevano.ch>
2020-05-26 10:04:20 +0200

    Rimossa animazione di attesa nella pagina di recupero password.

--- 5_Sito o applicativo/assets/js/pages/forgot-password.js
+++ 5_Sito o applicativo/assets/js/pages/forgot-password.js
@@ -3,31 +3,9 @@ $("#forgot-password").on("submit", (e) => {
 
   let email = $("#email").val();
 
-  $("#btn-forgot-password")
-    .prop("disabled", true)
-    .html('<div class="spinner-border"></div>');
   $.post("forgot-password", {
     email,
-  })
-    .then((data) => {
-      $("#error-message").hide();
-      $("#success-message")
-        .text("Email di recupero inviata con successo!")
-        .show();
-    })
-    .catch((err) => {
-      $("#success-message").hide();
-      if (err.status == 400) {
-        $("#error-message").text("Inserisci una email valida!").show();
-      } else {
-        $("#error-message")
-          .text("Impossibile collegarsi al server e-mail, riprova più tardi!")
-          .show();
-      }
-    })
-    .then(() => {
-      $("#btn-forgot-password")
-        .prop("disabled", false)
-        .html("Recupera la password");
-    });
+  });
+
+  $("#success-message").show();
 });
